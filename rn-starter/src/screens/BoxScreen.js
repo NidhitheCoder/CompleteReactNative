5d3e69e7880ab8baa6fb09f99d39045c3c2e6193
NidhitheCoder <nidhpv@gmail.com>
2020-11-28 16:13:46 +0530

    Little position exercises

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -3,10 +3,17 @@ import { View, StyleSheet, Text } from "react-native";
 
 const BoxScreen = () => {
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.textOneStyle}>Child Element #1</Text>
-      <Text style={styles.textTwoStyle}>Child Element #2</Text>
-      <Text style={styles.textThreeStyle}>Child Element #3</Text>
+    <View>
+      <View style={styles.viewStyle}>
+        <Text style={styles.textOneStyle}>Child Element #1</Text>
+        <Text style={styles.textTwoStyle}>Child Element #2</Text>
+        <Text style={styles.textThreeStyle}>Child Element #3</Text>
+      </View>
+      <View style={styles.ParentStyle}>
+        <View style={styles.viewOneStyle}></View>
+        <View style={styles.viewTwoStyle}></View>
+        <View style={styles.viewThreeStyle}></View>
+      </View>
     </View>
   );
 };
@@ -18,11 +25,12 @@ const styles = StyleSheet.create({
     // alignItems:'center',
     // flexDirection:"row",
     // justifyContent: "space-around",
-    height: 200
+    height: 200,
+    marginBottom: 30
   },
   textOneStyle: {
     borderWidth: 3,
-    borderColor: "red",
+    borderColor: "red"
     // alignSelf:"flex-start"
     // flex:4
   },
@@ -31,7 +39,7 @@ const styles = StyleSheet.create({
     borderColor: "blue",
     // alignSelf:"center"
     // flex:1
-    
+
     // position:'absolute',
     // top:0,
     // bottom:0,
@@ -39,13 +47,38 @@ const styles = StyleSheet.create({
     // right:0,
     // for above 5 line we can write like this \/
     ...StyleSheet.absoluteFillObject,
-    fontSize:20
+    fontSize: 20
   },
   textThreeStyle: {
     borderWidth: 3,
-    borderColor: "red",
+    borderColor: "red"
     // alignSelf:"flex-end"
     // flex:1
+  },
+
+  ParentStyle: {
+    borderWidth: 3,
+    borderColor: "black",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height:200
+  },
+  viewOneStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: "cyan"
+  },
+  viewTwoStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: "purple",
+    // marginTop:50,
+    top:50
+  },
+  viewThreeStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: "green"
   }
 });
 
