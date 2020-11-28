dba5f19952ba111e206c8dc0552beeb1f922f602
NidhitheCoder <nidhpv@gmail.com>
2020-11-28 15:24:49 +0530

    Flex Box  and its properties

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -4,7 +4,9 @@ import { View, StyleSheet, Text } from "react-native";
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Inside Box</Text>
+      <Text style={styles.textOneStyle}>Child Element #1</Text>
+      <Text style={styles.textTwoStyle}>Child Element #2</Text>
+      <Text style={styles.textThreeStyle}>Child Element #3</Text>
     </View>
   );
 };
@@ -12,12 +14,29 @@ const BoxScreen = () => {
 const styles = StyleSheet.create({
   viewStyle: {
     borderWidth: 3,
-    borderColor: "black"
+    borderColor: "black",
+    // alignItems:'center',
+    // flexDirection:"row",
+    // justifyContent: "space-around",
+    height: 200
   },
-  textStyle: {
-    borderWidth: 1,
+  textOneStyle: {
+    borderWidth: 3,
     borderColor: "red",
-    margin: 20
+    alignSelf:"flex-start"
+    // flex:4
+  },
+  textTwoStyle: {
+    borderWidth: 3,
+    borderColor: "red",
+    alignSelf:"center"
+    // flex:1
+  },
+  textThreeStyle: {
+    borderWidth: 3,
+    borderColor: "red",
+    alignSelf:"flex-end"
+    // flex:1
   }
 });
 
