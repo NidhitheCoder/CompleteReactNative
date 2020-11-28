aa426ee903df569aa4672edeb80576bdff7423c9
NidhitheCoder <nidhpv@gmail.com>
2020-11-28 13:28:32 +0530

    Box object model demo added

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -3,24 +3,26 @@ import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/ComponentsScreen";
 import ListScreen from "./src/screens/ListScreen";
-import ImageScreen from './src/screens/ImageScreen';
-import CounterScreen from './src/screens/CounterScreen';
-import ColorScreen from './src/screens/ColorScreen';
-import SquareScreen from './src/screens/SquareScreen';
-import SquareScreenWithState from './src/screens/SquareScreenWithState';
-import TextScreen from './src/screens/TextScreen';
+import ImageScreen from "./src/screens/ImageScreen";
+import CounterScreen from "./src/screens/CounterScreen";
+import ColorScreen from "./src/screens/ColorScreen";
+import SquareScreen from "./src/screens/SquareScreen";
+import SquareScreenWithState from "./src/screens/SquareScreenWithState";
+import TextScreen from "./src/screens/TextScreen";
+import BoxScreen from "./src/screens/BoxScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Components: ComponentsScreen,
     List: ListScreen,
-    Image:ImageScreen,
-    Counter:CounterScreen,
-    Color:ColorScreen,
+    Image: ImageScreen,
+    Counter: CounterScreen,
+    Color: ColorScreen,
     Square: SquareScreen,
-    SquareState:SquareScreenWithState,
-    Text:TextScreen
+    SquareState: SquareScreenWithState,
+    Text: TextScreen,
+    Box: BoxScreen
   },
   {
     initialRouteName: "Home",
